817a1fdc0b0d1e24ffeaf0a93e056e92e28ce94e
Basit Oketunde <basitoketunde@basits-mbp.home>
2022-01-14 11:54:03 +0000

    initial setup

---
+++ animeListAPI/src/main/java/com/example/animeListAPI/AnimeListApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.animeListAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeListApiApplication.class, args);
+	}
+
+}
---
+++ animeListAPI/src/test/java/com/example/animeListAPI/AnimeListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animeListAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
